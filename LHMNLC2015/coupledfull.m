30788c56579c42d0b1955c183b8230c492a0149e
Flávio Ribeiro <flavioluiz@gmail.com>
2015-03-10 10:33:49 +0100

    correction of disclaimer

--- LHMNLC2015/coupledfull.m
+++ LHMNLC2015/coupledfull.m
@@ -1,12 +1,12 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%																   %
+%                                                                  %
 %   Flavio Luiz Cardoso-Ribeiro: http://github.com/flavioluiz/     %
-%			ISAE-Supaero   / Instituto Tecnologico de Aeronautica  %
-%								CNPq   - Brazil 				   %
-%																   %
+%           ISAE-Supaero   / Instituto Tecnologico de Aeronautica  %
+%                               CNPq   - Brazil                    %
+%                                                                  %
 %    This project is part of ANR Project HAMECMOPSYS:              %
-%					https://hamecmopsys.ens2m.fr/ 				   %
-%																   %
+%                  https://hamecmopsys.ens2m.fr/                   %
+%                                                                  %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% this file show some results of the full coupled system
--- LHMNLC2015/test_eb.m
+++ LHMNLC2015/test_eb.m
@@ -1,12 +1,12 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%																   %
+%                                                                  %
 %   Flavio Luiz Cardoso-Ribeiro: http://github.com/flavioluiz/     %
-%			ISAE-Supaero   / Instituto Tecnologico de Aeronautica  %
-%								CNPq   - Brazil 				   %
-%																   %
+%           ISAE-Supaero   / Instituto Tecnologico de Aeronautica  %
+%                               CNPq   - Brazil                    %
+%                                                                  %
 %    This project is part of ANR Project HAMECMOPSYS:              %
-%					https://hamecmopsys.ens2m.fr/ 				   %
-%																   %
+%                  https://hamecmopsys.ens2m.fr/                   %
+%                                                                  %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Euler-Bernoulli beam test file:
